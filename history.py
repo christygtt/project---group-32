d3c7e5e20a343df0095fdb903abf63c8cced3c9f
Evans Asare <eaa250@msstate.edu>
2024-11-15 00:27:01 -0600

    Added methods(title only) to the history class

--- history.py
+++ history.py
@@ -1,4 +1,28 @@
 class OrderHistory:
 
     def __init__(self):
-        sample = ""
+        self.databaseName = "methods.db"
+        self.connection = sqlite3.connect(self.databaseName)
+        self.cursor = self.connection.cursor()
+    
+
+    def viewHistory(self, userID):
+
+    
+    def viewOrder(self, userID, orderID):
+
+        return order_details
+
+    
+import random
+
+    def createOrder(self, userID, orderID):
+         
+        return orderID
+
+
+    def addOrderItems(self, userID, orderID):
+    
+
+
+    def closeConnection(self):
